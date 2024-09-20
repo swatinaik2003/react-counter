a0910ac5ea67af9e31bc70c28a71591780d69345
swatinaik2003 <swati622005@gmail.com>
2024-09-20 13:45:29 +0530

    initially commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+
+const AddButton = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  const handleAddClick = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="count">{count}</h1>
+      <div className="button-container">
+        <button onClick={handleDecrement} className="decrement-button">
+          -
+        </button>
+        <button onClick={handleAddClick} className="add-button">
+          Add
+        </button>
+        <button onClick={handleIncrement} className="increment-button">
+          +
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default AddButton;
